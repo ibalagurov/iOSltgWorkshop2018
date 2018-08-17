c1386b328a42cc52e718d65a64344b20a2ff61e1
ibalagurov <optoinfo@mail.ru>
2018-08-18 00:35:25 +0300

    first raw test version

--- Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/BlueLibraryUITests.swift
+++ Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/BlueLibraryUITests.swift
@@ -9,28 +9,39 @@
 import XCTest
 
 class BlueLibraryUITests: XCTestCase {
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        let albumsToolbar = app.toolbars["Albums toolbar"]
+        let undo = albumsToolbar.buttons["Undo"]
+        while undo.isEnabled {
+            undo.tap()
+        }
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDeleteAlbum() {
+        let scroller = app.otherElements["Albums scroller"]
+        let scrollViews = scroller.scrollViews.otherElements
+        let albumsToolbar = app.toolbars["Albums toolbar"]
+        let delete = albumsToolbar.buttons["Delete"]
+        let undo = albumsToolbar.buttons["Undo"]
+        
+        scrollViews.element(boundBy: 0).tap()
+        XCTAssertEqual(scrollViews.count, 4)
+        XCTAssert(!undo.isEnabled)
+        
+        delete.tap()
+        
+        XCTAssertEqual(scrollViews.count, 3)
+        XCTAssert(undo.isEnabled)
     }
     
 }
