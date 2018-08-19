1bf61b5f85d57c6e4e92e9c18fd3247207662ad3
ibalagurov <optoinfo@mail.ru>
2018-08-19 17:48:42 +0300

    add action to screen object

--- Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/Screens.swift
+++ Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/Screens.swift
@@ -10,6 +10,7 @@ import XCTest
 
 class MainScreen {
     let app = XCUIApplication()
+    let defaultTimeout: TimeInterval = 10
     
     var scroller: XCUIElement { return app.otherElements["Albums scroller"] }
     var scrollViews: XCUIElementQuery { return scroller.scrollViews.otherElements }
@@ -17,4 +18,29 @@ class MainScreen {
     var albumInfo: XCUIElement { return app.tables["Albums information"] }
     var delete: XCUIElement { return albumsToolbar.buttons["Delete"] }
     var undo: XCUIElement { return albumsToolbar.buttons["Undo"] }
+    var tableTextElements: [XCUIElement] { return albumInfo.cells.allElementsBoundByAccessibilityElement }
+    
+    func albumWithIndex(index: Int) -> XCUIElement {
+        let element = scrollViews.element(boundBy: index)
+        return element
+    }
+    
+    func selectAlbumWithIndex(index: Int) -> Void {
+        let element = albumWithIndex(index: index)
+        let exists = element.waitForExistence(timeout: defaultTimeout)
+        XCTAssert(exists)
+        element.tap()
+    }
+    
+    func tableTexts() -> [(title: String, value: String)]{
+        let cellTitleValue = tableTextElements.map({
+            (title: $0.staticTexts["cell_title"].label, value: $0.staticTexts["cell_value"].label)
+        })
+        return cellTitleValue
+    }
+    
+    func rowValueByTitle(title: String) -> String {
+        let value = tableTexts().filter({$0.title == title})[0].value
+        return value
+    }
 }
