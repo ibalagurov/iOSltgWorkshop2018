3392f000225b6ab39e9c8e771e493f2a6d78ca7e
ibalagurov <optoinfo@mail.ru>
2018-08-20 12:43:07 +0300

    add error messages to asserts

--- Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/BlueLibraryUITests.swift
+++ Project 03 - BlueLibrarySwift/BlueLibrarySwiftUITests/BlueLibraryUITests.swift
@@ -16,13 +16,15 @@ class BlueLibraryUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        
-        // UI tests spend a lot of time on animations
-        UIView.setAnimationsEnabled(false)
-        XCTContext.runActivity(named: "Launch the application") {
-            _ in app.launch()
+        XCTContext.runActivity(named: "Launch the application") { _ in
+            // In UI tests it is usually best to stop immediately when a failure occurs.
+            continueAfterFailure = false
+            
+            // UI tests spend a lot of time on animations
+            UIView.setAnimationsEnabled(false)
+            
+            app.launch()
+            
             // portrait orientation is default
             XCUIDevice.shared.orientation = .portrait
         }
@@ -30,7 +32,7 @@ class BlueLibraryUITests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
-        XCTContext.runActivity(named: "Drop state and close application") { _ in
+        XCTContext.runActivity(named: "Drop state and close the application") { _ in
             while mainScreen.undo.isEnabled {
                 mainScreen.undo.tap()
             }
@@ -40,8 +42,8 @@ class BlueLibraryUITests: XCTestCase {
     
     func testDeleteAlbum() {
         XCTContext.runActivity(named: "There are 5 albums and undo button is disabled") { _ in
-            XCTAssertEqual(mainScreen.scrollViews.count, 5)
-            XCTAssert(!mainScreen.undo.isEnabled)
+            XCTAssertEqual(mainScreen.albumsList.count, 5, "Albums list should have 5 items")
+            XCTAssert(!mainScreen.undo.isEnabled, "Undo button should be disabled")
         }
         
         XCTContext.runActivity(named: "Delete selected album") { _ in
@@ -49,14 +51,14 @@ class BlueLibraryUITests: XCTestCase {
         }
         
         XCTContext.runActivity(named: "There are should be 4 albums and undo button should be enabled") { _ in
-            XCTAssertEqual(mainScreen.scrollViews.count, 4)
-            XCTAssert(mainScreen.undo.isEnabled)
+            XCTAssertEqual(mainScreen.albumsList.count, 4, "Albums list should have 4 items")
+            XCTAssert(mainScreen.undo.isEnabled, "Undo button should be enabled")
         }
     }
     
     func testDeleteAllAlbums() {
         XCTContext.runActivity(named: "There are 5 albums") { _ in
-            XCTAssertEqual(mainScreen.scrollViews.count, 5)
+            XCTAssertEqual(mainScreen.albumsList.count, 5, "Albums list should have 5 items")
         }
         
         XCTContext.runActivity(named: "Delete all albums") { _ in
@@ -64,16 +66,16 @@ class BlueLibraryUITests: XCTestCase {
         }
         
         XCTContext.runActivity(named: "Shouldn't be any albums, undo button is enabled and delete button is disabled") { _ in
-            XCTAssertEqual(mainScreen.scrollViews.count, 0)
-            XCTAssert(mainScreen.undo.isEnabled)
-            XCTAssert(!mainScreen.delete.isEnabled)
+            XCTAssertEqual(mainScreen.albumsList.count, 0, "Albums list should be empty")
+            XCTAssert(mainScreen.undo.isEnabled, "Undo button should be denabled")
+            XCTAssert(!mainScreen.delete.isEnabled, "Delete button should be disabled")
         }
     }
     
     func testUndoAlbumDeletion() {
         XCTContext.runActivity(named: "There are 4 albums and one was deleted") { _ in
             mainScreen.delete.tap()
-            XCTAssertEqual(mainScreen.scrollViews.count, 4)
+            XCTAssertEqual(mainScreen.albumsList.count, 4, "Albums list should have 4 items")
         }
         
         XCTContext.runActivity(named: "Undo deletion") { _ in
@@ -81,16 +83,16 @@ class BlueLibraryUITests: XCTestCase {
         }
         
         XCTContext.runActivity(named: "Should be 5 albums and undo button is disabled") { _ in
-            XCTAssertEqual(mainScreen.scrollViews.count, 5)
-            XCTAssert(!mainScreen.undo.isEnabled)
+            XCTAssertEqual(mainScreen.albumsList.count, 5, "Albums list should have 5 items")
+            XCTAssert(!mainScreen.undo.isEnabled, "Undo button should be disabled")
         }
     }
     
     func testSwipeAlbumView() {
         XCTContext.runActivity(named: "There are 5 albums: first is visible, last isn't visible") { _ in
             mainScreen.scroller.swipeRight()
-            XCTAssert(mainScreen.scrollViews.element(boundBy: 0).isHittable)
-            XCTAssert(!mainScreen.scrollViews.element(boundBy: 4).isHittable)
+            XCTAssert(mainScreen.albumsList.element(boundBy: 0).isHittable, "First album should be visible")
+            XCTAssert(!mainScreen.albumsList.element(boundBy: 4).isHittable, "Last(4th) album shouldn't be visible")
         }
         
         XCTContext.runActivity(named: "Scroll albums to the end of the list") { _ in
@@ -98,16 +100,16 @@ class BlueLibraryUITests: XCTestCase {
         }
         
         XCTContext.runActivity(named: "First album shouldn't be visible, the last one last is visible") { _ in
-            XCTAssert(!mainScreen.scrollViews.element(boundBy: 0).isHittable)
-            XCTAssert(mainScreen.scrollViews.element(boundBy: 4).isHittable)
+            XCTAssert(!mainScreen.albumsList.element(boundBy: 0).isHittable, "First album shouldn't be visible")
+            XCTAssert(mainScreen.albumsList.element(boundBy: 4).isHittable, "Last(4th) album should be visible")
         }
     }
     
     func testLandscapeOrientation() {
         XCTContext.runActivity(named: "There are 5 albums: first is visible, last isn't visible") { _ in
             mainScreen.scroller.swipeRight()
-            XCTAssert(mainScreen.scrollViews.element(boundBy: 0).isHittable)
-            XCTAssert(!mainScreen.scrollViews.element(boundBy: 4).isHittable)
+            XCTAssert(mainScreen.albumsList.element(boundBy: 0).isHittable, "First album should be visible")
+            XCTAssert(!mainScreen.albumsList.element(boundBy: 4).isHittable, "Last(4th) album shouldn't be visible")
         }
         
         XCTContext.runActivity(named: "Change device orientation to landscape") { _ in
@@ -115,8 +117,8 @@ class BlueLibraryUITests: XCTestCase {
         }
         
         XCTContext.runActivity(named: "First and last albums should be visible") { _ in
-            XCTAssert(mainScreen.scrollViews.element(boundBy: 0).isHittable)
-            XCTAssert(mainScreen.scrollViews.element(boundBy: 4).isHittable)
+            XCTAssert(mainScreen.albumsList.element(boundBy: 0).isHittable, "First album should be visible")
+            XCTAssert(mainScreen.albumsList.element(boundBy: 4).isHittable, "Last(4th) album should be visible")
         }
     }
     
@@ -127,7 +129,7 @@ class BlueLibraryUITests: XCTestCase {
         
         XCTContext.runActivity(named: "Album should be 'Poker Face'") { _ in
             let album = mainScreen.rowValueByTitle(title: "Album")
-            XCTAssertEqual(album, "Poker Face")
+            XCTAssertEqual(album, "Poker Face", "Second album should be 'Poker Face'")
         }
         
         XCTContext.runActivity(named: "Select 'U2' album") { _ in
@@ -136,7 +138,7 @@ class BlueLibraryUITests: XCTestCase {
         
         XCTContext.runActivity(named: "Album should be 'Staring at the Sun'") { _ in
             let album = mainScreen.rowValueByTitle(title: "Album")
-            XCTAssertEqual(album, "Staring at the Sun")
+            XCTAssertEqual(album, "Staring at the Sun", "4th album should be 'Poker Face'")
         }
     }
     
@@ -147,7 +149,7 @@ class BlueLibraryUITests: XCTestCase {
         
         XCTContext.runActivity(named: "Check all album information fields") { _ in
             let tableRows = mainScreen.tableTexts()
-            let expectedRows = ["Album": "Iridescent", "Artist": "Linkin Park", "Year": "2000", "Genre": "Pop"]
+            let expectedRows = ["Album":"Iridescent", "Artist":"Linkin Park", "Year":"2000", "Genre":"Pop"]
             for (title, value) in tableRows {
                 XCTAssertEqual(expectedRows[title], value)
             }
